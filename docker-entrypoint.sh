6aa8835e87cc6917117562c9e77149ab69070a10
Giovanni Toffetti <toff@zhaw.ch>
2015-09-30 09:59:20 +0200

    confd using the right file for init

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -84,13 +84,13 @@ EOSQL
         exit 1
       fi
 
-      cp /tmp/cluster.cnf /etc/mysql/conf.d/cluster.cnf
+#      cp /tmp/cluster.cnf /etc/mysql/conf.d/cluster.cnf
 
-      sed -i -e "s|wsrep_sst_auth \= \"sstuser:changethis\"|wsrep_sst_auth = ${WSREP_SST_USER}:${WSREP_SST_PASSWORD}|" /etc/mysql/conf.d/cluster.cnf
+      sed -i -e "s|wsrep_sst_auth \= \"sstuser:changethis\"|wsrep_sst_auth = ${WSREP_SST_USER}:${WSREP_SST_PASSWORD}|" /etc/confd/mysql/templates/cluster.cnf.tmpl
 
       WSREP_NODE_ADDRESS=${COREOS_PRIVATE_IPV4}
       if [ -n "$WSREP_NODE_ADDRESS" ]; then
-        sed -i -e "s|^#wsrep_node_address \= .*$|wsrep_node_address = ${WSREP_NODE_ADDRESS}|" /etc/confd/mysql/cluster.cnf.tmpl
+        sed -i -e "s|^#wsrep_node_address \= .*$|wsrep_node_address = ${WSREP_NODE_ADDRESS}|" /etc/confd/mysql/templates/cluster.cnf.tmpl
       fi
 
       if [ -z "$WSREP_CLUSTER_ADDRESS" -o "$WSREP_CLUSTER_ADDRESS" == "gcomm://" ]; then
@@ -102,11 +102,11 @@ EOSQL
 #        WSREP_CLUSTER_ADDRESS="gcomm://"`./kubectl describe service zurmo-galera-cluster | grep -E "Endpoints.*3306.*" | cut -f 3 | sed -e "s|:3306||g"`
       fi 
   
-      # Ok, now that we went through the trouble of building up a nice
-      # cluster address string, regex the conf file with that value 
-      if [ -n "$WSREP_CLUSTER_ADDRESS" -a "$WSREP_CLUSTER_ADDRESS" != "gcomm://" ]; then
-        sed -i -e "s|wsrep_cluster_address \= gcomm://|wsrep_cluster_address = ${WSREP_CLUSTER_ADDRESS}|" /etc/mysql/conf.d/cluster.cnf
-      fi
+#      # Ok, now that we went through the trouble of building up a nice
+#      # cluster address string, regex the conf file with that value 
+#      if [ -n "$WSREP_CLUSTER_ADDRESS" -a "$WSREP_CLUSTER_ADDRESS" != "gcomm://" ]; then
+#        sed -i -e "s|wsrep_cluster_address \= gcomm://|wsrep_cluster_address = ${WSREP_CLUSTER_ADDRESS}|" /etc/mysql/conf.d/cluster.cnf
+#      fi
 
       echo "CREATE USER '${WSREP_SST_USER}'@'localhost' IDENTIFIED BY '${WSREP_SST_PASSWORD}';" >> "$tempSqlFile"
       echo "GRANT RELOAD, LOCK TABLES, REPLICATION CLIENT ON *.* TO '${WSREP_SST_USER}'@'localhost';" >> "$tempSqlFile"
@@ -119,8 +119,10 @@ EOSQL
   chown -R mysql:mysql "$DATADIR"
 fi
 
+export ETCD_ENDPOINT=${ETCD_ENDPOINT:-172.17.42.1:4001}
+
 # Try to make initial configuration every 5 seconds until successful
-until confd -onetime -node $ETCD_ENDPOINT -config-file /etc/confd/mysql/cluster.cnf.tmpl; do
+until confd -onetime -node $ETCD_ENDPOINT -config-file /etc/confd/mysql/conf.d/zurmo_galera_cluster.toml; do
     echo "[mysql-cluster] waiting for confd to create initial mysql-cluster configuration."
     sleep 5
 done
@@ -132,5 +134,5 @@ exec "$@"
 
 # Put a continual polling `confd` process into the background to watch
 # for changes every 10 seconds
-confd -interval 10 -node $ETCD_ENDPOINT -config-file /etc/confd/mysql/zurmo_galera_cluster.toml &
+confd -interval 10 -node $ETCD_ENDPOINT -config-file /etc/confd/mysql/conf.d/zurmo_galera_cluster.toml &
 echo "[mysql-cluster] confd is now monitoring etcd for c
