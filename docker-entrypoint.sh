29501153f3ee73612619d45c70f526c0c2363a41
Giovanni Toffetti <toff@zhaw.ch>
2015-10-06 17:02:23 +0200

    search db cluster using components rather than service

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -88,18 +88,12 @@ EOSQL
 
       sed -i -e "s|wsrep_sst_auth \= \"sstuser:changethis\"|wsrep_sst_auth = ${WSREP_SST_USER}:${WSREP_SST_PASSWORD}|" /etc/confd/mysql/templates/cluster.cnf.tmpl
 
+      export COREOS_PRIVATE_IPV4=${COREOS_PRIVATE_IPV4:-172.17.42.1}
+
       WSREP_NODE_ADDRESS=${COREOS_PRIVATE_IPV4}
       if [ -n "$WSREP_NODE_ADDRESS" ]; then
         sed -i -e "s|^#wsrep_node_address \= .*$|wsrep_node_address = ${WSREP_NODE_ADDRESS}|" /etc/confd/mysql/templates/cluster.cnf.tmpl
       fi
-
-      
-  
-#      # Ok, now that we went through the trouble of building up a nice
-#      # cluster address string, regex the conf file with that value 
-#      if [ -n "$WSREP_CLUSTER_ADDRESS" -a "$WSREP_CLUSTER_ADDRESS" != "gcomm://" ]; then
-#        sed -i -e "s|wsrep_cluster_address \= gcomm://|wsrep_cluster_address = ${WSREP_CLUSTER_ADDRESS}|" /etc/mysql/conf.d/cluster.cnf
-#      fi
 
       echo "CREATE USER '${WSREP_SST_USER}'@'localhost' IDENTIFIED BY '${WSREP_SST_PASSWORD}';" >> "$tempSqlFile"
       echo "GRANT RELOAD, LOCK TABLES, REPLICATION CLIENT ON *.* TO '${WSREP_SST_USER}'@'localhost';" >> "$tempSqlFile"
@@ -121,25 +115,24 @@ if [ $GALERA_CLUSTER_NODE_ID == 1 ]; then
 #   fi 
     cp /etc/confd/mysql/templates/cluster.cnf.tmpl /etc/mysql/conf.d/cluster.cnf
     sed -i -e "s|^wsrep_cluster_address \= .*$|wsrep_cluster_address = ${WSREP_CLUSTER_ADDRESS}|" /etc/mysql/conf.d/cluster.cnf
+    set -- "$@" --wsrep-new-cluster
 else
     # Try to make initial configuration every 5 seconds until successful
-    until confd -verbose -debug -onetime -node $ETCD_ENDPOINT -config-file /etc/confd/mysql/conf.d/ zurmo_galera_cluster.toml; do
+    until confd -verbose -debug -onetime -node $ETCD_ENDPOINT -config-file /etc/confd/mysql/conf.d/zurmo_galera_cluster.toml; do
       echo "[mysql-cluster] waiting for confd to create initial mysql-cluster configuration."
       sleep 5
-done
+    done    
 fi 
 
 echo "[mysql-cluster] mysql-cluster configuration is now:"
 cat /etc/mysql/conf.d/cluster.cnf
+exec "$@"
 
-chmod +x /restart_mysql.sh
-exec "mysqld_safe"
+#chmod +x /restart_mysql.sh
 
 # FIXME: I am currently disabling discovery because I still do not know how to make node 1 restart and join an existing cluster
 ## Put a continual polling `confd` process into the background to watch
 ## for changes every 10 seconds
 #confd -interval 10 -node $ETCD_ENDPOINT -config-file /etc/confd/mysql/conf.d/zurmo_galera_cluster.toml &
 #echo "[mysql-cluster] confd is now monitoring etcd for changes"
-
-tail -f /var/log/mysql.log
 
